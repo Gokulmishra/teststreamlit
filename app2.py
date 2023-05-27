b41f1500ef5e59cdcfe25cb5d1a22e6a2c2765ba
Gokul Ballava Mishra <67966130+Gokulmishra@users.noreply.github.com>
2023-05-27 23:16:28 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,7 +8,7 @@ camera = cv2.VideoCapture(0)
 
 while run:
     _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
