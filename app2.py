719ca16f2cb5421c21e356e01cf2b29d6abe50ca
Gokul Ballava Mishra <67966130+Gokulmishra@users.noreply.github.com>
2023-05-27 23:25:30 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,14 +1,13 @@
-import cv2
-import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
 
-st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
 
-while run:
-    _, frame = camera.read()
-#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:]
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
